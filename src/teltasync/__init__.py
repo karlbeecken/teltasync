bef2babd95e23528231917e33e0bde2dc1e1ce90
Karl Beecken <karl@beecken.berlin>
2026-05-02 18:29:36 +0200

    bump version to 0.3.0

--- src/teltasync/__init__.py
+++ src/teltasync/__init__.py
@@ -8,7 +8,7 @@ from teltasync.exceptions import (
 )
 from teltasync.teltasync import Teltasync
 
-__version__ = "0.2.0"
+__version__ = "0.3.0"
 __all__ = [
     "Teltasync",
     "TeltonikaException",
